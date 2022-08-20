b6834c8c3758b79cbdc768e804cae7a201ab72db
Duc Nguyen <minhduc140583@gmail.com>
2022-08-20 19:01:49 +0700

    Add export

---
+++ export/exporter.go
@@ -0,0 +1,94 @@
+package export
+
+import (
+	"context"
+	c "github.com/core-go/cassandra"
+	"github.com/gocql/gocql"
+	"reflect"
+)
+
+func NewExportRepository(db *gocql.ClusterConfig, modelType reflect.Type,
+	buildQuery func(context.Context) (string, []interface{}),
+	transform func(context.Context, interface{}) string,
+	write func(p []byte) (n int, err error),
+	close func() error,
+) (*Exporter, error) {
+	return NewExporter(db, modelType, buildQuery, transform, write, close)
+}
+func NewExportAdapter(db *gocql.ClusterConfig, modelType reflect.Type,
+	buildQuery func(context.Context) (string, []interface{}),
+	transform func(context.Context, interface{}) string,
+	write func(p []byte) (n int, err error),
+	close func() error,
+) (*Exporter, error) {
+	return NewExporter(db, modelType, buildQuery, transform, write, close)
+}
+func NewExportService(db *gocql.ClusterConfig, modelType reflect.Type,
+	buildQuery func(context.Context) (string, []interface{}),
+	transform func(context.Context, interface{}) string,
+	write func(p []byte) (n int, err error),
+	close func() error,
+) (*Exporter, error) {
+	return NewExporter(db, modelType, buildQuery, transform, write, close)
+}
+
+func NewExporter(db *gocql.ClusterConfig, modelType reflect.Type,
+	buildQuery func(context.Context) (string, []interface{}),
+	transform func(context.Context, interface{}) string,
+	write func(p []byte) (n int, err error),
+	close func() error,
+) (*Exporter, error) {
+	fieldsIndex, err := c.GetColumnIndexes(modelType)
+	if err != nil {
+		return nil, err
+	}
+	return &Exporter{DB: db, modelType: modelType, Write: write, Close: close, fieldsIndex: fieldsIndex, Transform: transform, BuildQuery: buildQuery}, nil
+}
+
+type Exporter struct {
+	DB          *gocql.ClusterConfig
+	modelType   reflect.Type
+	fieldsIndex map[string]int
+	Transform   func(context.Context, interface{}) string
+	BuildQuery  func(context.Context) (string, []interface{})
+	Write       func(p []byte) (n int, err error)
+	Close       func() error
+}
+
+func (s *Exporter) Export(ctx context.Context) error {
+	query, p := s.BuildQuery(ctx)
+	session, err := s.DB.CreateSession()
+	if err != nil {
+		return err
+	}
+	q := session.Query(query, p...)
+	err = q.Exec()
+	if err != nil {
+		return err
+	}
+	return s.ScanAndWrite(ctx, q.Iter(), s.modelType)
+}
+
+func (s *Exporter) ScanAndWrite(ctx context.Context, iter *gocql.Iter, structType reflect.Type) error {
+	defer s.Close()
+	columns := c.GetColumns(iter.Columns())
+	for {
+		initModel := reflect.New(structType).Interface()
+		r := c.StructScan(initModel, columns, s.fieldsIndex, -1)
+		if !iter.Scan(r...) {
+			return nil
+		} else {
+			err1 := s.TransformAndWrite(ctx, s.Write, initModel)
+			if err1 != nil {
+				return err1
+			}
+		}
+	}
+	return nil
+}
+
+func (s *Exporter) TransformAndWrite(ctx context.Context, write func(p []byte) (n int, err error), model interface{}) error {
+	line := s.Transform(ctx, model)
+	_, er := write([]byte(line))
+	return er
+}
---
+++ export/fixedlength_formatter.go
@@ -0,0 +1,148 @@
+package export
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/big"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type FixedLengthFormatter struct {
+	modelType  reflect.Type
+	formatCols map[int]*FixedLength
+}
+type FixedLength struct {
+	Format string
+	Scale  int
+	Length int
+}
+
+func GetIndexes(modelType reflect.Type, tagName string) (map[int]*FixedLength, error) {
+	ma := make(map[int]*FixedLength, 0)
+	if modelType.Kind() != reflect.Struct {
+		return ma, errors.New("bad type")
+	}
+	for i := 0; i < modelType.NumField(); i++ {
+		field := modelType.Field(i)
+		tagValue := field.Tag.Get(tagName)
+		tagLength := field.Tag.Get("length")
+		if len(tagLength) > 0 {
+			length, err := strconv.Atoi(tagLength)
+			if err != nil || length < 0 {
+				return ma, err
+			}
+			v := &FixedLength{Length: length}
+			tagScale, sOk := field.Tag.Lookup("scale")
+			if sOk {
+				scale, err := strconv.Atoi(tagScale)
+				if err == nil {
+					v.Scale = scale
+				}
+			}
+			if len(tagValue) > 0 {
+				if strings.Contains(tagValue, "dateFormat:") {
+					tagValue = strings.ReplaceAll(tagValue, "dateFormat:", "")
+				} else if sOk == false && strings.Contains(tagValue, "scale:") {
+					tagValue = strings.ReplaceAll(tagValue, "scale:", "")
+					scale, err1 := strconv.Atoi(tagValue)
+					if err1 != nil {
+						return ma, err1
+					}
+					v.Scale = scale
+				}
+				v.Format = tagValue
+			}
+			ma[i] = v
+		}
+	}
+	return ma, nil
+}
+
+func NewFixedLengthFormatter(modelType reflect.Type) (*FixedLengthFormatter, error) {
+	formatCols, err := GetIndexes(modelType, "format")
+	if err != nil {
+		return nil, err
+	}
+	return &FixedLengthFormatter{modelType: modelType, formatCols: formatCols}, nil
+}
+
+func (f *FixedLengthFormatter) Format(ctx context.Context, model interface{}) string {
+	return ToFixedLength(model, f.formatCols)
+}
+func ToFixedLength(model interface{}, formatCols map[int]*FixedLength) string {
+	arr := make([]string, 0)
+	sumValue := reflect.Indirect(reflect.ValueOf(model))
+	for i := 0; i < sumValue.NumField(); i++ {
+		format, ok := formatCols[i]
+		if ok {
+			field := sumValue.Field(i)
+			kind := field.Kind()
+			var value string
+			if kind == reflect.Ptr && field.IsNil() {
+				value = FixedLengthString(format.Length, value)
+			} else {
+				v := field.Interface()
+				if kind == reflect.Ptr {
+					v = reflect.Indirect(reflect.ValueOf(v)).Interface()
+					kind = field.Elem().Kind()
+				}
+				if s, okS := v.(string); okS {
+					value = FixedLengthString(format.Length, s)
+				} else {
+					if d, okD := v.(time.Time); okD {
+						value = d.Format(format.Format)
+					} else {
+						if kind == reflect.Struct {
+							if v2 := reflect.Indirect(reflect.ValueOf(v)); v2.NumField() == 1 {
+								f := v2.Field(0)
+								fv := f.Interface()
+								k := f.Kind()
+								if k == reflect.Ptr {
+									fv = reflect.Indirect(reflect.ValueOf(fv)).Interface()
+								}
+								if sv, ok := fv.(big.Float); ok {
+									prec := 2
+									if format.Scale > 0 {
+										prec = format.Scale
+									}
+									value = sv.Text('f', prec)
+								} else if svi, ok := fv.(big.Int); ok {
+									value = svi.Text(10)
+								} else {
+									value = fmt.Sprint(v)
+								}
+							} else {
+								value = fmt.Sprint(v)
+								if len(value) > format.Length {
+									value = strings.TrimSpace(value)
+								}
+								if len(format.Format) > 0 {
+									value = fmt.Sprintf(format.Format, value)
+								}
+							}
+						} else {
+							value = fmt.Sprint(v)
+							if len(value) > format.Length {
+								value = strings.TrimSpace(value)
+							}
+							if len(format.Format) > 0 {
+								value = fmt.Sprintf(format.Format, value)
+							}
+						}
+					}
+					value = FixedLengthString(format.Length, value)
+				}
+			}
+			arr = append(arr, value)
+		}
+	}
+	return strings.Join(arr, "") + "\n"
+}
+func FixedLengthString(length int, str string) string {
+	verb := fmt.Sprintf("%%%d.%ds", length, length)
+	return fmt.Sprintf(verb, str)
+}
