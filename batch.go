d6543e792fbf05bcf3e4b82705774b23487e8455
Duc Nguyen <minhduc140583@gmail.com>
2024-01-06 22:58:41 +0700

    Add BuildToSaveBatch

--- batch.go
+++ batch.go
@@ -3,6 +3,7 @@ package cassandra
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 func InterfaceSlice(slice interface{}) ([]interface{}, error) {
@@ -85,4 +86,75 @@ func BuildToUpdateBatchWithVersion(table string, models interface{}, versionInde
 		stmts = append(stmts, s)
 	}
 	return stmts, nil
+}
+func BuildToSaveBatch(table string, models interface{}, options ...*Schema) (string, []interface{}, error) {
+	s := reflect.Indirect(reflect.ValueOf(models))
+	if s.Kind() != reflect.Slice {
+		return "", nil, fmt.Errorf("models must be a slice")
+	}
+	slen := s.Len()
+	if slen <= 0 {
+		return "", nil, nil
+	}
+	buildParam := BuildParam
+	var cols []*FieldDB
+	// var schema map[string]FieldDB
+	if len(options) > 0 && options[0] != nil {
+		cols = options[0].Columns
+		// schema = options[0].Fields
+	} else {
+		first := s.Index(0).Interface()
+		modelType := reflect.TypeOf(first)
+		m := CreateSchema(modelType)
+		cols = m.Columns
+	}
+	placeholders := make([]string, 0)
+	args := make([]interface{}, 0)
+	i := 1
+	icols := make([]string, 0)
+	for _, fdb := range cols {
+		if fdb.Insert {
+			icols = append(icols, fdb.Column)
+		}
+	}
+	for j := 0; j < slen; j++ {
+		model := s.Index(j).Interface()
+		mv := reflect.ValueOf(model)
+		values := make([]string, 0)
+		for _, fdb := range cols {
+			if fdb.Insert {
+				f := mv.Field(fdb.Index)
+				fieldValue := f.Interface()
+				isNil := false
+				if f.Kind() == reflect.Ptr {
+					if reflect.ValueOf(fieldValue).IsNil() {
+						isNil = true
+					} else {
+						fieldValue = reflect.Indirect(reflect.ValueOf(fieldValue)).Interface()
+					}
+				}
+				if isNil {
+					icols = append(icols, fdb.Column)
+					values = append(values, "null")
+				} else {
+					v, ok := GetDBValue(fieldValue, fdb.Scale)
+					if ok {
+						values = append(values, v)
+					} else {
+						values = append(values, buildParam(i))
+						i = i + 1
+						args = append(args, fieldValue)
+					}
+				}
+			}
+		}
+		x := "(" + strings.Join(values, ",") + ")"
+		placeholders = append(placeholders, x)
+	}
+	query := fmt.Sprintf(fmt.Sprintf("insert into %s (%s) values %s",
+		table,
+		strings.Join(icols, ","),
+		strings.Join(placeholders, ","),
+	))
+	return query, args, nil
 }
